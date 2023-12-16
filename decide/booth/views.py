1c19d44233e7736fab0c3101653a315a35b057fc
[[alblopnov]] <[[a.lopezbenjumea@gmail.com]]>
2023-12-16 20:47:02 +0100

    fixed the token not beeing generated

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -2,6 +2,7 @@ import json
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
+from rest_framework.authtoken.models import Token
 
 from base import mods
 
@@ -14,7 +15,11 @@ class BoothView(TemplateView):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
         context['user'] = self.request.user
-
+        if self.request.user.is_authenticated:
+            token, created = Token.objects.get_or_create(user=self.request.user)
+            context['token'] = token.key
+        else:
+            context['token'] = ''
         try:
             r = mods.get('voting', params={'id': vid})
             # Casting numbers to string to manage in javascript with BigInt
