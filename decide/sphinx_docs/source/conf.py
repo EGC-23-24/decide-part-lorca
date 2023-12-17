8b27d97b0b0755a5b70906bbce0ff13ef2c8b254
alejandromd <aleprieto02@gmail.com>
2023-12-17 22:36:55 +0100

    feat: add all docs to GitHub Pages #329

--- decide/sphinx_docs/source/conf.py
+++ decide/sphinx_docs/source/conf.py
@@ -25,7 +25,8 @@ release = '0.0.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'myst_parser',
 ]
 
 templates_path = ['_templates']
