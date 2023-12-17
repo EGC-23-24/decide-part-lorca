a09c5573e4b1b912852759a4df6ab1c753bc1000
[[alvarobernal2412]] <[[alvarobc2412@gmail.com]]>
2023-12-17 15:28:42 +0000

    fix: Change issue.yml #338

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -20,5 +20,6 @@ class BoothTestCase(BaseTestCase):
         # Se va a probar con el numero 10000 pues en las condiciones actuales en las que nos encontramos no parece posible que se genren 10000 votaciones diferentes
         response = self.client.get('/booth/10000')
         self.assertEqual(response.status_code, 301)
-
+    def test_that_always_fail(self):
+        self.assertEqual(1, 2)
        
